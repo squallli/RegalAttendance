2edf8bc016e26b9c8df63a16cdda600efadfeec4
Regal System <rgsystem@regalde-imac.local>
2016-02-05 10:52:01 +0800

    Initial Commit

---
+++ RegalAttendance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RegalAttendance
+//
+//  Created by Regal System on 2016/2/5.
+//  Copyright © 2016年 Regal System. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
